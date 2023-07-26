aa7418216b8467a3e9cd8da96e7a54d11b2e3c67
Victor Berton <bertonvictor@hotmail.com>
2023-07-26 16:46:39 -0300

    first commit

---
+++ src/test/java/vb/tasks/functionaltest/TasksTest.java
@@ -0,0 +1,93 @@
+package vb.tasks.functionaltest;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TasksTest {
+
+	public WebDriver accessWebsite() {
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://localhost:8001/tasks");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}
+	
+	@Test
+	public void addTask() {
+		WebDriver driver = accessWebsite();
+		
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			
+			driver.findElement(By.id("task")).sendKeys("Teste Selenium");
+			driver.findElement(By.id("dueDate")).sendKeys("20/10/2025");
+			
+			driver.findElement(By.id("saveButton")).click();
+			
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+		} finally {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void taskWithoutDescriptionValidation() {
+		WebDriver driver = accessWebsite();
+		
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			
+			driver.findElement(By.id("dueDate")).sendKeys("20/10/2025");
+			
+			driver.findElement(By.id("saveButton")).click();
+			
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Fill the task description", message);
+		} finally {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void taskWithoutDateValidation() {
+		WebDriver driver = accessWebsite();
+		
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			
+			driver.findElement(By.id("task")).sendKeys("Teste Selenium");
+			
+			driver.findElement(By.id("saveButton")).click();
+			
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Fill the due date", message);
+		} finally {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void taskWithInvalidDateValidation() {
+		WebDriver driver = accessWebsite();
+		
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			
+			driver.findElement(By.id("task")).sendKeys("Teste Selenium");
+			
+			driver.findElement(By.id("dueDate")).sendKeys("20/10/2020");
+			
+			driver.findElement(By.id("saveButton")).click();
+			
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Due date must not be in past", message);
+		} finally {
+			driver.quit();
+		}
+	}
+}
